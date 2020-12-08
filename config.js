92fd81ee7c8134355c434e914d5b6d7e27f18d2b
catrinax <72537185+catrinax@users.noreply.github.com>
2020-12-08 22:46:21 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* Store configuration values */
 module.exports = {
     TWILIO_ACCOUNT_SID: 'AC753dc8a90e2c4bef1cf423cf3d02a404',
-    TWILIO_AUTH_TOKEN: '236fddb39859bb26d00c2d7c435c951a',
+    TWILIO_AUTH_TOKEN: 'your_auth_token',
     PHONE_NR: '+61450490703'
 };
